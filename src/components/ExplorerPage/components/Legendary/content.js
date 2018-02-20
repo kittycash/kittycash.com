467996f2b89466690a1b477583e79981b3764f17
128-95 <128-95@protonmail.com>
2018-02-20 21:11:21 +0100

    Legend Synth is sold

--- src/components/ExplorerPage/components/Legendary/content.js
+++ src/components/ExplorerPage/components/Legendary/content.js
@@ -1,6 +1,6 @@
 import Satoshi from './imgs/Satoshi_sold-01.png';
 import Potter from './imgs/Harry_sold-01.png';
-import Synth from './imgs/Synth-01.png';
+import Synth from './imgs/Synth_sold-01.png';
 import genie from './imgs/Genie.png';
 
 export default [{
@@ -12,10 +12,11 @@ export default [{
   sold: 'nEbCGNS58vchzQU1CqD1\neDWBX1amEccZ9s'
 }, {
   name: 'Synth',
-  priceBTC: '?',
-  priceSKY: '?',
+  priceBTC: '1.2',
+  priceSKY: '576',
   description: 'You see that split? That\'s how Skycoin prevents chain splits.',
   img: Synth,
+  sold: 'mNJgBmnjqNNjafGKJdZ\nxTQ12GNjcWdQ12S'
 }, {
   name: '???',
   priceBTC: '?',
