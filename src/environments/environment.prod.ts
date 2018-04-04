fa69781e51c4245ab5b9b6cb677a1de941abf7de
Brad Kendall <me@bradk.xyz>
2018-04-04 08:31:07 +0100

    Update config to new staging server.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://pre-staging.kittycash.com',
+  serverUrl: 'https://staging-api.kittycatscatarat.club',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
