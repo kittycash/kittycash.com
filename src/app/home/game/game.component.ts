da12e853f643532ab01c61592157e85fa86f0e1c
Brad Kendall <me@bradk.xyz>
2018-04-26 17:45:23 +0100

    Update game component to respond to height events from kitty game.

--- src/app/home/game/game.component.ts
+++ src/app/home/game/game.component.ts
@@ -10,13 +10,33 @@ import { environment } from '@env/environment';
 export class GameComponent implements OnInit {
  
   gameUrl: string;
+  iframeHeight: number = 0;
 
   constructor() { 
-  	this.gameUrl = environment.serverUrl + "/scoreboard/game?show_scoreboard_link=true";
+    this.gameUrl = environment.serverUrl + "/scoreboard/game?show_scoreboard_link=true";
   }
 
  
   ngOnInit() {
-    
-  }
+      let __this = this;
+     // Listen to messages from parent window
+    this.bindEvent(window, 'message', function (e:any) {
+      if (e && e.data && e.data.command && e.data.command == "update height")
+      {
+          if (e.data.height && e.data.height > 0)
+          {
+            __this.iframeHeight = e.data.height;
+          }
+      }
+    });
+  } 
+
+   private bindEvent(element:any, eventName:any, eventHandler:any) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, eventHandler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + eventName, eventHandler);
+        }
+    }
+
 }
